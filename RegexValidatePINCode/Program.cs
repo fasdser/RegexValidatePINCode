cb2ff12c79676b9dfd5dc5f422eba9d7325166a5
fasdser <107477645+fasdser@users.noreply.github.com>
2022-11-02 18:06:24 +0300

    Add project files.

---
+++ RegexValidatePINCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexValidatePINCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidatePin("123"));
+            Console.ReadKey();
+        }
+
+        public static bool ValidatePin(string pin)
+        {
+            string value = pin.Replace(" ", "");
+            string pattern = @"^\d{4}$|^\d{6}$";
+            var regex = new Regex(pattern);
+
+
+
+            return regex.IsMatch(pin) ? true : false;
+        }
+
+        public static bool ValidatePin1(string pin)
+        {
+            return Regex.IsMatch(pin, @"^(\d{6}|\d{4})\z");
+        }
+
+        public static bool ValidatePin2(string pin) =>
+   (pin.Length == 4 || pin.Length == 6) && pin.All(Char.IsDigit);
+    }
+}
